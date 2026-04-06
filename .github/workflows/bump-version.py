324b0ff99898c2f75a3c5d0d0e2e64a160d5fe0e
Ole Herman Schumacher Elgesem <ole@northern.tech>
2026-04-06 04:37:21 +0200

    One last try
    Signed-off-by: Ole Herman Schumacher Elgesem <ole@northern.tech>

--- .github/workflows/bump-version.py
+++ .github/workflows/bump-version.py
@@ -5,20 +5,11 @@ with open("package.json", "r") as f:
 
 data = json.loads(data)
 
-before = data["version"]
-
 version = data["version"].split(".")
 version[2] = str(int(version[2]) + 1)
 version = ".".join(version)
 
 data["version"] = version
 
-after = data["version"]
-
 with open("package.json", "w") as f:
     f.write(json.dumps(data, indent=2) + "\n")
-
-commit = f"package.json: Bumped version from {before} to {after}"
-
-with open("commit_message.txt", "w") as f:
-    f.write(commit)
